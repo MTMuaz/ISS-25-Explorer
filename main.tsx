a2fc3b29419d5454bd11e72c9d368a539b959c93
MD:Mustafa Tahmid Muaz <muaztahmid99@gmail.com>
2025-10-03 11:56:23 +0600

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+
+<script type="module" src="/src/main.jsx"></script>
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
